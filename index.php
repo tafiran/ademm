f8a07973687b34f19e622c39efa179f408583b6f
tafiran <40736319+tafiran@users.noreply.github.com>
2021-09-15 00:49:03 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="container">
+           <nav id="navigation">
+
+                <a href="#" class="logo">jan czizikow</a>
+                <a aria-label="mobile menu" class="nav-toggle">
+                <span></span><span></span><span></span></a>
+
+                <ul class="menu">
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#exp">works</a></li>
+                    <li><a href="#contact">contact</a></li>
+                </ul>
+
+            </nav>
+    </div>
+</body>
+
+<footer>
+    <?php include "assets/scripts.php" ?>
+</footer>
+
+</html>
